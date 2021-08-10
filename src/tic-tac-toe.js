0616ec9825760505f4f03c06ab889e9160f5f63c
Igor Desh <desh_gambler@mail.ru>
2021-08-10 12:55:52 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,66 @@
 class TicTacToe {
     constructor() {
-
+        this.matrix = [[null,null,null,],
+                       [null,null,null,],
+                       [null,null,null,]]
+        this.currentPlayer = 'x'  
+        this.winner = null      
     }
 
     getCurrentPlayerSymbol() {
-
+        //should return x or o
+        return this.currentPlayer
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        //should properly update class state (change current player, update marks storage etc.)
+        this.matrix[rowIndex][columnIndex] = this.getCurrentPlayerSymbol()
+        this.currentPlayer = (this.getCurrentPlayerSymbol() === 'x') ? 'o' : 'x'
     }
 
     isFinished() {
-
+        //should return true if game is finished (e.g. there is a winner or it is a draw)
+        return (this.winner !== null || this.isDraw()) ? true : false
     }
 
     getWinner() {
-
+        //should return winner symbol (x or o) or null if there is no winner yet
+        if (
+            this.matrix[0][0] == this.currentPlayer && this.matrix[0][1] == this.currentPlayer && this.matrix[0][2] == this.currentPlayer
+            || this.matrix[1][0] == this.currentPlayer && this.matrix[1][1] == this.currentPlayer && this.matrix[1][2] == this.currentPlayer
+            || this.matrix[2][0] == this.currentPlayer && this.matrix[2][1] == this.currentPlayer && this.matrix[2][2] == this.currentPlayer
+            || this.matrix[0][0] == this.currentPlayer && this.matrix[1][1] == this.currentPlayer && this.matrix[2][2] == this.currentPlayer
+            || this.matrix[2][0] == this.currentPlayer && this.matrix[1][1] == this.currentPlayer && this.matrix[0][2] == this.currentPlayer
+            || this.matrix[0][0] == this.currentPlayer && this.matrix[1][0] == this.currentPlayer && this.matrix[2][0] == this.currentPlayer
+            || this.matrix[0][1] == this.currentPlayer && this.matrix[1][1] == this.currentPlayer && this.matrix[2][1] == this.currentPlayer
+            || this.matrix[0][2] == this.currentPlayer && this.matrix[1][2] == this.currentPlayer && this.matrix[2][2] == this.currentPlayer
+        ) {
+            this.winner = this.currentPlayer;
+            return this.winner;
+        }
     }
 
     noMoreTurns() {
-
+        //should return true if there is no more fields to place a x or o
+        return (this.matrix.filter(item => item.some((mark) => mark === null)).length !== 0) ? false : true
     }
 
     isDraw() {
+        //should return true if there is no more turns and no winner
+        return (this.noMoreTurns() && this.winner === null) ? true : false
 
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+    //should return matrix[row][col] value (if any) or null
+    return (this.noMoreTurns()) ? null : this.matrix[rowIndex][colIndex]
     }
 }
 
 module.exports = TicTacToe;
+
+
+
+  
+  
+   
